4c459caac172b6d74002056e93379415bd396cf1
Sami <s.m.sami125@gmail.com>
2022-10-23 22:21:53 +0500

    create event

--- src/pagesComponent/createorUpdateEvent/index.js
+++ src/pagesComponent/createorUpdateEvent/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Grid } from '@material-ui/core';
 
 import { makeStyles } from '@material-ui/core/styles';
@@ -16,9 +16,53 @@ const useStyles = makeStyles((theme) => ({
 }));
 export default function Index(props) {
   const classes = useStyles();
+  const [data, setData] = useState({
+    name: '',
+    tags: [],
+    description: '',
+    image: '',
+    startDate: '',
+    endDate: '',
+    location: '',
+    venue: '',
+    price: '',
+    address: '',
+    phone: '',
+    email: '',
+    facebook: '',
+    twitter: '',
+    insta: '',
+    linkdin: '',
+    snapchat: '',
+    whatsApp: '',
+    sponsors: [],
+    speakers: [],
+    schedule: [],
+  });
+
+  const [coverFile, setCoverFile] = useState(null);
+  const [tag, setTag] = useState('');
+
+  const [zoom, setZoom] = useState(1);
+  const [cropper, setCropper] = useState();
+  const [showImageCropper, setShowImageCropper] = useState({
+    active: false,
+    file: null,
+    fileName: '',
+  });
+  const [showSponsorCropper, setShowSponsorCropper] = useState({
+    active: false,
+    file: null,
+    fileName: '',
+  });
+  const [showSpeakerCropper, setShowSpeakerCropper] = useState({
+    active: false,
+    file: null,
+    fileName: '',
+  });
   return (
     <Grid container direction="column">
-      index
+      <Grid item style={{ marginTop: '2em' }}></Grid>
     </Grid>
   );
 }
