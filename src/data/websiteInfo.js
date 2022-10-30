c1646c997756978387860fb3e7f2ef47c02cb412
Sami <s.m.sami125@gmail.com>
2022-10-30 17:16:18 +0500

    name change

--- src/data/websiteInfo.js
+++ src/data/websiteInfo.js
@@ -1,8 +1,8 @@
-export const websiteTitle = 'Deiven The-Seminar';
+export const websiteTitle = 'Driven: The-Seminar';
 export const websiteDescription =
   'Grab Your FREE Ticket to the Higher Purpose Tour Before Our Limited Seating Books Out!';
 export const websiteKeywords =
   'Free Online Appointment Scheduling, Contacts and network building Software, business networking, tappio, sharing contact details, single share, neworking with proud, professional networking, website builder, all in one solution, Meeting, Business card, Academy, Community, Movement, connect digitally,starup münchen, startup Dresden, connecting people, tap and connect, NFC, IOS and mobile application, future of networking, Saving nature with technology, nature, ';
 
-export const tokenKey = 'deiven-jwt';
+export const tokenKey = 'driven-jwt';
 export const maximumFileSize = 5 * 1024 * 1024;
